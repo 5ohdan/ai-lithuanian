b9f3d696ea02081d7cdeaec1174700a0b61d412d
Bohdan Zakovinko <b.zakovinko@gmail.com>
2025-02-22 11:58:03 +0400

    Remove localStorage-related data management functions
    - Deleted `data.ts` file containing local storage utility functions
    - Removed unused import of `toast` from sonner in key-form page

--- src/app/(api-key)/key-form/page.tsx
+++ src/app/(api-key)/key-form/page.tsx
@@ -1,6 +1,5 @@
 import { cookies } from "next/headers";
 import Link from "next/link";
-import { toast } from "sonner";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
