8e4945d5ec8579094ab240941d9dc796d3ac10b9
Bohdan Zakovinko <b.zakovinko@gmail.com>
2025-04-04 11:39:01 +0400

    Implement authentication check in HomePage component
    - Add authentication logic to HomePage to redirect unauthenticated users to the sign-in page using Clerk's auth method.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 export default async function HomePage() {
+  const { userId, redirectToSignIn } = await auth();
+
+  if (!userId) return redirectToSignIn();
+
   redirect("/new-set");
 }
