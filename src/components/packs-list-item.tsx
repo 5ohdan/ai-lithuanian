1a6973ead1262dc8deb2c3c2dc17978b91b1f813
Bohdan Zakovinko <b.zakovinko@gmail.com>
2025-04-27 14:19:42 +0400

    Refactor PacksListItem component to use Link for navigation
    - Replaced useRouter with Link component for improved navigation to pack details.
    - Streamlined the code by removing unnecessary click handlers and enhancing the overall structure of the PacksListItem component.

--- src/components/packs-list-item.tsx
+++ src/components/packs-list-item.tsx
@@ -1,7 +1,7 @@
 import { motion } from "motion/react";
 import type { StoredPack, StoredBriefPack } from "~/lib/schemas";
 import { CardContent, Card } from "./ui/card";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 type PackItemProps = {
   pack: StoredPack | StoredBriefPack;
@@ -9,8 +9,6 @@ type PackItemProps = {
 };
 
 export function PacksListItem({ pack, index }: PackItemProps) {
-  const router = useRouter();
-
   return (
     <motion.div
       key={pack.id}
@@ -42,38 +40,37 @@ export function PacksListItem({ pack, index }: PackItemProps) {
         },
       }}
     >
-      <Card
-        className="cursor-pointer bg-neutral-100/85"
-        onClick={() => router.push(`/packs/${pack.id}`)}
-      >
-        <CardContent className="p-4">
-          <div className="flex items-center justify-between">
-            <h2 className="mb-2 text-xl font-semibold">{pack.title}</h2>
-            <p className="mb-2 text-sm text-gray-500/75">
-              Created: {new Date(pack.createdAt).toLocaleDateString()}
-            </p>
-          </div>
-          <div className="flex flex-wrap gap-2">
-            {pack.set.map((word, index) => (
-              <motion.span
-                key={index}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{
-                  type: "spring",
-                  stiffness: 100,
-                  damping: 20,
-                  mass: 0.5,
-                  delay: 0.2 + index * 0.05,
-                }}
-                className="rounded-md bg-white px-2 py-1 text-sm"
-              >
-                {word.original}
-              </motion.span>
-            ))}
-          </div>
-        </CardContent>
-      </Card>
+      <Link href={`/packs/${pack.id}`} prefetch={true}>
+        <Card className="cursor-pointer bg-neutral-100/85">
+          <CardContent className="p-4">
+            <div className="flex items-center justify-between">
+              <h2 className="mb-2 text-xl font-semibold">{pack.title}</h2>
+              <p className="mb-2 text-sm text-gray-500/75">
+                Created: {new Date(pack.createdAt).toLocaleDateString()}
+              </p>
+            </div>
+            <div className="flex flex-wrap gap-2">
+              {pack.set.map((word, index) => (
+                <motion.span
+                  key={index}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{
+                    type: "spring",
+                    stiffness: 100,
+                    damping: 20,
+                    mass: 0.5,
+                    delay: 0.2 + index * 0.05,
+                  }}
+                  className="rounded-md bg-white px-2 py-1 text-sm"
+                >
+                  {word.original}
+                </motion.span>
+              ))}
+            </div>
+          </CardContent>
+        </Card>
+      </Link>
     </motion.div>
   );
 }
