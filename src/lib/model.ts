e92d7e98c28e43d402a99a3095b949193218f4a3
Bohdan <b.zakovinko@gmail.com>
2025-07-30 09:43:22 +0400

    refactor: update Google AI model initialization

--- src/lib/model.ts
+++ src/lib/model.ts
@@ -1,14 +1,14 @@
 import { google } from "@ai-sdk/google";
 
-const googleAI = google("gemini-2.5-flash-preview-04-17");
+const googleAI = google("gemini-2.5-flash");
 
-export type Providers = 'google'
+export type Providers = "google";
 
 export const getModel = (provider: Providers) => {
   switch (provider) {
-    case 'google':
-      return googleAI
+    case "google":
+      return googleAI;
     default:
-      return googleAI
+      return googleAI;
   }
-}
+};
